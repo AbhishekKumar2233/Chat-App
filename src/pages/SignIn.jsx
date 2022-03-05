2cc1cb9e6bb5f267a49903412dd07e9135d45b95
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-05 16:36:47 +0530

    signin page is styled

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 // import { Container, Grid, Row, Col, Panel } from "rsuite/dist/styles/rsuite-default.css";
-import { Container, Grid, Col, Row, Panel } from "rsuite";
+import {
+  Container,
+  Grid,
+  Col,
+  Row,
+  Panel,
+  Button,
+  ButtonToolbar,
+  IconButton,
+  FacebookOfficialIcon
+} from "rsuite";
 // import Col from "rsuite/lib/Carousel";
 
 const SignIn = () => {
   return (
     <Container>
-      <Grid>
+      <Grid className="mt-page">
         <Row>
           <Col xs={24} md={12} mdOffset={6}>
             <Panel>
               <div className="text-center">
                 <h2>Welcome to Chat App</h2>
                 <p>Dear User</p>
+              </div>
+
+              <div className="mt-3">
+                <ButtonToolbar>
+                  <Button block color="blue" appearance="primary">
+                    Continue with Facebook
+                  </Button>
+                  <Button block color="green" appearance="primary">
+                    Continue with Google
+                  </Button>
+                </ButtonToolbar>
               </div>
             </Panel>
           </Col>
