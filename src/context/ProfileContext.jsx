954350f5b617e3ddf7e5a0de15d68af83c4fb1ea
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-06 23:38:34 +0530

    after sign in user redirected to Home page

--- src/context/ProfileContext.jsx
+++ src/context/ProfileContext.jsx
@@ -1,9 +1,27 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { auth, database } from "../mics/config";
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profile] = useState(false);
+  const [profile, setProfile] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authObj) => {
+      if (authObj) {
+        database.ref(`/profiles/${authObj.uid}`).on("value", (snap) => {});
+        const data = {
+          uid: authObj.uid,
+          email: authObj.email
+        };
+
+        setProfile(data);
+      } else {
+        setProfile(null);
+      }
+    });
+  }, []);
+
   return (
     <ProfileContext.Provider value={profile}>
       {children}
