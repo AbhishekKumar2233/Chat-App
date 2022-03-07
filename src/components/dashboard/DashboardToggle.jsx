ab4fc16f9b6a0264240e654daf8ab2fa9aa09fa3
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-07 16:58:36 +0530

    drawer is added to app

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Button, Icon, Drawer } from "rsuite";
+import { useModelState } from "../../mics/custom-hook";
+import Dashboard from "./Index";
 
 export default function DashboardToggle() {
+  const { isOpen, close, open } = useModelState();
+
   return (
     <div>
-      <Button block color="blue" appearance="primary">
+      <Button block color="blue" appearance="primary" onClick={open}>
         {/* <Icon icon="dashboard" /> */}
         Dashboard
       </Button>
-      <Drawer placement="left">Hyy</Drawer>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+        Hyy
+      </Drawer>
     </div>
   );
 }
