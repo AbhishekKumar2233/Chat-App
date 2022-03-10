e12fc3a54006d4531c8ae58c44262870852e13a4
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-11 00:48:42 +0530

    the chatroom is as link using react router

--- src/components/rooms/ChatRoomList.jsx
+++ src/components/rooms/ChatRoomList.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Nav, Loader } from "rsuite";
 import RoomItem from "./RoomItem";
 import { useRooms } from "../../context/RoomContext";
+import { Link, useLocation } from "react-router-dom";
 
 export default function ChatRoomList({ aboveElHeight }) {
   const rooms = useRooms();
+  const location = useLocation();
 
   return (
     <div>
@@ -16,6 +18,7 @@ export default function ChatRoomList({ aboveElHeight }) {
         style={{
           height: `calc(100% - ${aboveElHeight}px)`
         }}
+        activeKey={location.pathname}
       >
         {!rooms && (
           <Loader center vertical content="Loading" speed="slow" size="md" />
@@ -23,7 +26,12 @@ export default function ChatRoomList({ aboveElHeight }) {
         {rooms &&
           rooms.length > 0 &&
           rooms.map((room) => (
-            <Nav.Item key={room.id}>
+            <Nav.Item
+              componentClass={Link}
+              to={`/chat/${room.id}`}
+              key={room.id}
+              eventKey={`/chat/${room.id}`}
+            >
               <RoomItem room={room} />
             </Nav.Item>
           ))}
