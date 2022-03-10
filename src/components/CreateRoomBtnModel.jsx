fde0c53c7f91e8d5137cf02e5ea5694d1ad1f4a5
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-10 22:46:58 +0530

    Room can be created in database with validation

--- src/components/CreateRoomBtnModel.jsx
+++ src/components/CreateRoomBtnModel.jsx
@@ -1,12 +1,60 @@
-import React from "react";
-import { Modal, Button, Form, Input } from "rsuite";
+import firebase from "firebase";
+import React, { useState, useCallback, useRef } from "react";
+import { Modal, Button, Form, Input, Schema } from "rsuite";
 import { useModelState } from "../mics/custom-hook";
+import { database } from "../mics/config";
+
+//form validation
+const { StringType } = Schema.Types;
+
+const model = Schema.Model({
+  name: StringType().isRequired("Chat name is required"),
+  description: StringType().isRequired("Chat description is required")
+});
+
+const INITIAL_FORM = {
+  name: "",
+  description: ""
+};
 
 export default function CreateRoomBtnModel() {
   const { isOpen, open, close } = useModelState();
   const Description = React.forwardRef((props, ref) => (
     <Input {...props} as="textarea" ref={ref} />
   ));
+
+  const [formValue, setFormValue] = useState(INITIAL_FORM);
+  const [isLoading, setLoading] = useState(false);
+  const fromRef = useRef();
+
+  const onFormChange = useCallback((value) => {
+    setFormValue(value);
+  }, []);
+
+  //submit func
+  const onSubmit = async () => {
+    if (!fromRef.current.check()) {
+      return;
+    }
+    setLoading(true);
+
+    const newRoomdata = {
+      ...formValue,
+      createdAt: firebase.database.ServerValue.TIMESTAMP
+    };
+
+    try {
+      await database.ref("rooms").push(newRoomdata);
+
+      alert(`${formValue.name} has been created`);
+      setLoading(false);
+      setFormValue(INITIAL_FORM);
+      close();
+    } catch (err) {
+      setLoading(false);
+      alert(err);
+    }
+  };
 
   return (
     <div className="mt-1">
@@ -18,7 +66,13 @@ export default function CreateRoomBtnModel() {
           <Modal.Title>Create New Chat Room</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form fluid>
+          <Form
+            fluid
+            onChange={onFormChange}
+            formValue={formValue}
+            model={model}
+            ref={fromRef}
+          >
             <Form.Group>
               <Form.ControlLabel>Room Name</Form.ControlLabel>
               <Form.Control name="name" placeholder="Enter chat room name.." />
@@ -35,7 +89,12 @@ export default function CreateRoomBtnModel() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button block appearance="primary">
+          <Button
+            block
+            appearance="primary"
+            onClick={onSubmit}
+            disabled={isLoading}
+          >
             Create
           </Button>
         </Modal.Footer>
