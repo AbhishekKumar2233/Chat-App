5736d71ad8dbe39423dcb7f4399d7d913b631292
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-12 20:31:09 +0530

    user is able to send the message to database

--- src/components/chat-window/bottom/Index.jsx
+++ src/components/chat-window/bottom/Index.jsx
@@ -1,6 +1,9 @@
 import React, { useCallback, useState } from "react";
 import { InputGroup, Input } from "rsuite";
 import firebase from "firebase/app";
+import { useParams } from "react-router";
+import { useProfile } from "../../../context/ProfileContext";
+import { database } from "../../../mics/config";
 
 function assembleMessage(profile, chatId) {
   return {
@@ -17,16 +20,45 @@ function assembleMessage(profile, chatId) {
 
 export default function Bottom() {
   const [input, setInput] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const { chatId } = useParams();
+  const { profile } = useProfile();
 
   const onInputChange = useCallback((value) => {
     setInput(value);
   }, []);
 
-  const onSendClick = () => {
+  const onSendClick = async () => {
     if (input.trim() === "") {
       return;
     }
-    // const msgData = assembleMessage(profile, chatId);
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+    const updates = {};
+
+    const messageId = database.ref("message").push().key; //using this method we get unique key from real time database
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgData: messageId
+    };
+    setLoading(true);
+
+    try {
+      await database.ref().update(updates);
+      setInput("");
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      alert(err);
+    }
+  };
+
+  const onKeyDown = (ev) => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
   };
 
   return (
@@ -36,11 +68,13 @@ export default function Bottom() {
           placeholder="Write a new message.."
           value={input}
           onChange={onInputChange}
+          onKeyDown={onKeyDown}
         />
         <InputGroup.Button
           color="blue"
           appearance="primary"
           onClick={onSendClick}
+          disabled={isLoading}
         >
           Send
         </InputGroup.Button>
