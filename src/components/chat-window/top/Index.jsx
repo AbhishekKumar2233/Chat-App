f78c28e9348ff4b3d473aec7d44528ffee8a4627
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-12 14:52:50 +0530

    Go home in chat button for mobile device

--- src/components/chat-window/top/Index.jsx
+++ src/components/chat-window/top/Index.jsx
@@ -1,9 +1,32 @@
 import React, { memo } from "react";
+import ArrowLeftLineIcon from "@rsuite/icons/ArrowLeftLine";
+import { Link } from "react-router-dom";
 import { useCurrentRoom } from "../../../context/CurrentRoomContext";
+import { useMediaQuery } from "../../../mics/custom-hook";
 
 function Top() {
   const name = useCurrentRoom((v) => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery("(max-width: 992px");
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-item-center">
+        <h4>
+          {/* <Icon as={Link} to="/" icon="arrow-circle-left" size="2x" /> */}
+          <Link
+            to="/"
+            className={
+              isMobile
+                ? "d-inline-block p-0 mr-2 text-blue link-unstyled"
+                : "d-none"
+            }
+          >
+            <ArrowLeftLineIcon />
+          </Link>
+          <span className="text-disappear">{name}</span>
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export default memo(Top);
