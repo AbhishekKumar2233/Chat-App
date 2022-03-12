750ccb380ea77a96dd422670345aa22b518663c8
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-12 22:48:02 +0530

    error solved

--- src/components/chat-window/bottom/Index.jsx
+++ src/components/chat-window/bottom/Index.jsx
@@ -14,7 +14,7 @@ function assembleMessage(profile, chatId) {
       createdAt: profile.createdAt,
       ...(profile.avatar ? { avatar: profile.avatar } : {})
     },
-    createdAt: firebase.databse.ServerValue.TIMESTAMP
+    createdAt: firebase.database.ServerValue.TIMESTAMP
   };
 }
 
