2e16e55a2e9bd2b1a6860dd34c552455fdb09e88
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-13 15:59:01 +0530

    data passed to MessageItem from Intex Message

--- src/components/chat-window/messages/Index.jsx
+++ src/components/chat-window/messages/Index.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { database } from "../../../mics/config";
+import { transformToArrWithId } from "../../../mics/helpers";
+import MessageItem from "./MessageItem";
 
 export default function Message() {
-  return <div>Message Index</div>;
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messagesRef = database.ref("/messages");
+    messagesRef
+      .orderByChild("roomId")
+      .equalTo(chatId)
+      .on("value", (snap) => {
+        const data = transformToArrWithId(snap.val());
+        setMessages(data);
+      });
+
+    return () => {
+      messagesRef.off("value");
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No Message</li>}
+      {canShowMessages &&
+        messages.map((msg) => <MessageItem key={msg.id} messages={msg} />)}
+    </ul>
+  );
 }
---
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MessageItem() {
+  return <div>Message Index</div>;
+}
