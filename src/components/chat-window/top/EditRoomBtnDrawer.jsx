a3d08be7c5a167e9118d889f6c150f4647541572
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-14 23:05:05 +0530

    Input fields are added

--- src/components/chat-window/top/EditRoomBtnDrawer.jsx
+++ src/components/chat-window/top/EditRoomBtnDrawer.jsx
@@ -7,6 +7,9 @@ import { useCurrentRoom } from "../../../context/CurrentRoomContext";
 export default function EditRoomBtnDrawer() {
   const { open, close, isOpen } = useModelState();
 
+  const onNameSave = () => {};
+  const onDescription = () => {};
+
   const name = useCurrentRoom((v) => v.name);
   const description = useCurrentRoom((v) => v.description);
   return (
@@ -14,6 +17,32 @@ export default function EditRoomBtnDrawer() {
       <Button className="br-circle" size="sm" onClick={open}>
         Admin
       </Button>
+
+      <Drawer show={isOpen} onClose={close} placement="right">
+        <Drawer.Header>
+          <Drawer.Title>Edit Room</Drawer.Title>
+        </Drawer.Header>
+        <Drawer.Body>
+          <EditableInput
+            initialValue={name}
+            onSave={onNameSave}
+            label={<h6 classname="mb-2">Name</h6>}
+            emptyMsg="Name can not be Empty"
+          />
+          <EditableInput
+            as="textarea"
+            initialValue={description}
+            onSave={onDescription}
+            emptyMsg="Description can not be Empty"
+            label={<h6 classname="mb-2"> Description</h6>}
+          />
+        </Drawer.Body>
+        <Drawer.Footer>
+          <Button appearance="primary" block onClick={close}>
+            Close
+          </Button>
+        </Drawer.Footer>
+      </Drawer>
     </div>
   );
 }
