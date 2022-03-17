ba725ad5d5864094c3be2d9ab7b9a52f977ff24a
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-17 23:02:05 +0530

    Like msg btn is styled

--- src/components/chat-window/messages/IconBtnControl.jsx
+++ src/components/chat-window/messages/IconBtnControl.jsx
@@ -1,7 +1,44 @@
+import Icon from "@rsuite/icons/lib/Icon";
 import React from "react";
+import { Badge, IconButton, Tooltip, Whisper } from "rsuite";
 
-const IconBtnControl = () => {
-  return <div>Hyy</div>;
+const ConditionalBadge = ({ condition, children }) => {
+  return condition ? <Badge content={condition}>{children}</Badge> : children;
+};
+
+const IconBtnControl = ({
+  isVisible,
+  iconName,
+  tooltip,
+  onClick,
+  badgeContent,
+  ...props
+}) => {
+  return (
+    <div
+      className="ml-2"
+      style={{ visibility: isVisible ? "visible" : "hidden" }}
+    >
+      <ConditionalBadge condition={badgeContent}>
+        <Whisper
+          placement="top"
+          delay={0}
+          delayClose={0}
+          delayOpen={0}
+          trigger="hover"
+          speaker={<Tooltip>{tooltip}</Tooltip>}
+        >
+          <IconButton
+            {...props}
+            onClick={onClick}
+            circle
+            size="xs"
+            icon={<Icon icon={iconName} />}
+          />
+        </Whisper>
+      </ConditionalBadge>
+    </div>
+  );
 };
 
 export default IconBtnControl;
