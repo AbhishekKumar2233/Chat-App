70f4ec2ac00c1315512618c1d0ab24f5bc2d5b74
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-18 23:55:14 +0530

    working on upload files to database

--- src/components/chat-window/bottom/AttactmentBtnModal.jsx
+++ src/components/chat-window/bottom/AttactmentBtnModal.jsx
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { Button, InputGroup, Modal, Uploader } from "rsuite";
 import AttachmentIcon from "@rsuite/icons/Attachment";
 import { useModelState } from "../../../mics/custom-hook";
+import { storage } from "../../../mics/config";
+import { useParams } from "react-router";
 
 const MAX_FILE_SIZE = 1000 * 1024 * 5;
 
 export default function AttactmentBtnModel() {
+  const { chatId } = useParams;
   const { isOpen, open, close } = useModelState();
   const [fileList, setFileList] = useState([]);
+  const [isLoading, setLoading] = useState();
 
   const onChange = (fileArr) => {
     const filtered = fileArr
@@ -16,6 +20,23 @@ export default function AttactmentBtnModel() {
 
     setFileList(filtered);
   };
+  const onUpload = async () => {
+    try {
+      const UploadPromises = fileList.map((f) => {
+        return storage
+          .ref(`/chat/${chatId}`)
+          .child(Date.now() + f.name)
+          .put(f.blobFile, {
+            cacheControl: `public, max-age=${3600 * 24 * 3} `
+          });
+      });
+
+      const uploadSnapshots = await Promise.all(UploadPromises);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <>
       <InputGroup.Button onClick={open}>
@@ -33,13 +54,16 @@ export default function AttactmentBtnModel() {
             onChange={onChange}
             multiple
             listType="picture-text"
+            disabled={isLoading}
           />
         </Modal.Body>
         <Modal.Footer>
           <div className="text-right mt-2">
             <small>* only files less than 5 mb are allowed</small>
           </div>
-          <Button block>Send to chat</Button>
+          <Button block disabled={isLoading} onClick={onUpload}>
+            Send to chat
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
