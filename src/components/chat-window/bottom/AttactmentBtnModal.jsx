354e2e7b35fb45cd93c6740bd1bcdbda0545183f
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-18 23:35:27 +0530

    file upload modal style is done

--- src/components/chat-window/bottom/AttactmentBtnModal.jsx
+++ src/components/chat-window/bottom/AttactmentBtnModal.jsx
@@ -1,18 +1,40 @@
-import React from "react";
-import { Button, InputGroup, Modal } from "rsuite";
+import React, { useState } from "react";
+import { Button, InputGroup, Modal, Uploader } from "rsuite";
 import AttachmentIcon from "@rsuite/icons/Attachment";
 import { useModelState } from "../../../mics/custom-hook";
 
+const MAX_FILE_SIZE = 1000 * 1024 * 5;
+
 export default function AttactmentBtnModel() {
   const { isOpen, open, close } = useModelState();
+  const [fileList, setFileList] = useState([]);
+
+  const onChange = (fileArr) => {
+    const filtered = fileArr
+      .filter((el) => el.blobFile.size <= MAX_FILE_SIZE)
+      .slice(0, 5);
+
+    setFileList(filtered);
+  };
   return (
     <>
       <InputGroup.Button onClick={open}>
         <AttachmentIcon />
       </InputGroup.Button>
-      <Modal open={isOpen} close={close}>
+      <Modal open={isOpen} onClose={close}>
         <Modal.Header>Upload Files</Modal.Header>
-        <Modal.Body>Upload</Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Uploader
+            className="w-100"
+            autoUpload={false}
+            action=""
+            fileList={fileList}
+            onChange={onChange}
+            multiple
+            listType="picture-text"
+          />
+        </Modal.Body>
         <Modal.Footer>
           <div className="text-right mt-2">
             <small>* only files less than 5 mb are allowed</small>
