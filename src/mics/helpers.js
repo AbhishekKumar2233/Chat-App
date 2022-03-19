bfff06a40bdc71e1965fa2687e86468815f32d61
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-03-19 18:33:47 +0530

    custom hook is created for get msgs in group of dates

--- src/mics/helpers.js
+++ src/mics/helpers.js
@@ -49,3 +49,17 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
 
   return updates;
 }
+
+//creating hook for msg arr by date
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+    return result;
+  });
+}
